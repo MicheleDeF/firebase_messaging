24696a127b52b14e4768e08a3cbf308a2e545f6f
michele de falco <micheledefalco28@gmail.com>
2020-11-15 15:47:25 +0100

    second commit

---
+++ init.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAMNA7VISwz2-6YAv4lzB7AWtWCTC09WHc",
+  authDomain: "fcm-push-58be6.firebaseapp.com",
+  databaseURL: "https://fcm-push-58be6.firebaseio.com",
+  projectId: "fcm-push-58be6",
+  storageBucket: "fcm-push-58be6.appspot.com",
+  messagingSenderId: "41373202660",
+  appId: "1:41373202660:web:02b7353d4d3c68ebe254f1",
+  measurementId: "G-GC74P6N69V"
+};
+
+firebase.initializeApp(firebaseConfig);
---
+++ sendmessaging.php
@@ -0,0 +1,66 @@
+<?php
+
+if(isset($_GET['token'])){
+	
+$tokens = $_GET['token'];
+
+define('SERVER_API_KEY','AAAACaII-OQ:APA91bHnpH2lmM2eWzMGUTPWZNFrCnQrvEkLL_Tzr5Xw2IPowJfpjPCNEoJeI8-AIY0J7CFe8Oan_9EWJQqJv57Nr96yD3wZwePGHEhql8tIAM376VXiBIc2zZhmVhVxvSgBKzjx9QoQ');
+
+$header = [
+            'Authorization: key='.SERVER_API_KEY,
+			'Content-Type: Application/json'
+			
+          ];
+		  
+$notification = [
+        'title' => 'il blog di un programmatore ha un nuovo articolo',
+		'body'  => 'React JS, impostare l\'ambiente di lavoro',
+		'icon'  => 'https://www.ilblogdiunprogrammatore.it/image/favicon.ico',
+		'click_action' => 'https://www.ilblogdiunprogrammatore.it/39825-react-js-impostare-l-ambiente-di-lavoro.html'
+		];
+		  
+$payload = [
+            'notification' => $notification,
+			'to' => $tokens
+			];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($payload),
+  CURLOPT_HTTPHEADER => $header
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo "Notifica inviata con successo.";  
+}
+	
+	
+}else{
+	
+	echo "invalid token!";
+	
+}
+
+
+
+
+
+
+
+
+
+?>
+
+
+
