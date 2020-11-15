15618016f68026b8970ba91ee0ec1355793e4194
michele de falco <defalco_michele@libero.it>
2020-11-15 15:56:08 +0100

    Update init.js

--- init.js
+++ init.js
@@ -1,12 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAMNA7VISwz2-6YAv4lzB7AWtWCTC09WHc",
-  authDomain: "fcm-push-58be6.firebaseapp.com",
-  databaseURL: "https://fcm-push-58be6.firebaseio.com",
-  projectId: "fcm-push-58be6",
-  storageBucket: "fcm-push-58be6.appspot.com",
-  messagingSenderId: "41373202660",
-  appId: "1:41373202660:web:02b7353d4d3c68ebe254f1",
-  measurementId: "G-GC74P6N69V"
+var firebaseConfig= 
+{ 
+  apiKey:"<Api-Key>", 
+  authDomain:"<ID-Progetto>.firebaseapp.com", 
+  databaseURL:"https://<ID-Progetto>.firebaseio.com", 
+  projectId:"<ID-Progetto>", 
+  storageBucket:"<ID-Progetto>.appspot.com", 
+  messagingSenderId:"<Id-Mittente>", 
+  appId:"<App-Id>", 
+  measurementId:"<Measurement-Id>" 
 };
 
-firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
