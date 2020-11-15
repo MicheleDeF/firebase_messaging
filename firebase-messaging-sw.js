a53338be15ad58c2a993646247abb31f6381e2ce
michele de falco <defalco_michele@libero.it>
2020-11-15 17:10:51 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
- importScripts('https://www.gstatic.com/firebasejs/7.5.2/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/7.5.2/firebase-messaging.js');
- importScripts('./init.js');
+importScripts('https://www.gstatic.com/firebasejs/7.4.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.4.0/firebase-messaging.js');
+importScripts('init.js');
 
 const messaging = firebase.messaging();
 
